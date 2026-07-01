e7e7bf119cbff6019eed565b291b5dad9ac5ea2d
Justine Barlaan <justineocsanbarlaan@gmail.com>
2026-07-01 08:23:12 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/programs', function () {
+    return view('programs');
+});
+
+Route::get('/privacy-policy', function () {
+    return view('pages.privacy');
+});
+
+Route::get('/terms', function () {
+    return view('pages.terms');
+});

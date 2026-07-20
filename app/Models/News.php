1d563face229777bdcd07e280d8a6b94af289cd7
Justine Barlaan <justineocsanbarlaan@gmail.com>
2026-07-20 09:41:54 +0800

    Create News.php

---
+++ app/Models/News.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'body',
+        'image_path',
+        'category',
+        'is_published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true)->whereNotNull('published_at')->where('published_at', '<=', now());
+    }}

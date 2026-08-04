25f41d9acb2f2d8ca9800d83abdae7bc24c698c1
Justine Barlaan <justineocsanbarlaan@gmail.com>
2026-08-04 10:02:11 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -243,9 +243,9 @@
                 <span>Join Our Team</span>
                 <svg class="w-5 h-5 arrow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"/></svg>
             </a>
-            <a href="{{ url('/org-structure') }}" class="group inline-flex items-center justify-center gap-2 px-10 py-4 font-bold uppercase tracking-[0.1em] text-[13px] border-2 border-[#111827] text-[#111827]">
+            <a href="{{ url('/org-structure') }}" class="btn-wbr btn-wbr-dark">
                 <span>Org Structure</span>
-                <svg class="w-5 h-5 transition-transform duration-300 group-hover:translate-x-[5px]" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"/></svg>
+                <svg class="w-5 h-5 arrow" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"/></svg>
             </a>
         </div>
     </div>
